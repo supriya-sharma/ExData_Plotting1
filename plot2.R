4dcddd573cebefe14d677697ae7dc8b37c1b4da9
supriya7 <supriya7@bu.edu>
2015-06-07 19:05:38 -0400

    added plot2.R

--- plot2.R
+++ plot2.R
@@ -8,7 +8,7 @@ new_data <- house_data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 ##### Subset data for time and convert date time format
 datetime <- strptime(paste(new_data$Date, new_data$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
 
-####### define and plot global active power
+####### define and plot global active power in kilowatts
 globalActivePower <- as.numeric(new_data$Global_active_power)
 png("plot2.png", width=480, height=480)
 plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
